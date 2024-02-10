5173c3bb7e866b72e6f1e8c7d24a40e20b223e7d
JOÃO VITOR RODRIGUES MARTINEZ <3337.343@edu.fb.org.br>
2024-02-09 21:52:52 -0300

    Aula do dia 09/02 - onde fizemos uma pagina e puxamos variaveis de uma pagina para outra

--- Controllers/DepartmentsController.cs
+++ Controllers/DepartmentsController.cs
@@ -3,12 +3,12 @@ using MVC_Will.Models.ViewModel;
 using System.Collections.Generic;
 namespace MVC_Will.Controllers
 {
-    public class DepartmentController : Controller
+    public class DepartmentsController : Controller
     {
         public IActionResult Index()
         {
             List<Department> departments = new List<Department>();
-            departments.Add(new Department { Id= 1, Name = "Games" });
+            departments.Add(new Department { Id = 1, Name = "Games" });
             departments.Add(new Department { Id = 2, Name = "Consoles" });
             return View(departments);
         }
